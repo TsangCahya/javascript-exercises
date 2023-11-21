866d0c459a373932b6db407bd309615ccb752423
Tsang Cahya <67121407+TsangCahya@users.noreply.github.com>
2023-11-21 07:32:08 +0700

    adding solution removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...num) {
+    const baru= [];
+    for(let value of arr){
+        if(num.includes(value))continue;
+        baru.push(value);
+    }
+    return baru;
 };
 
 // Do not edit below this line
